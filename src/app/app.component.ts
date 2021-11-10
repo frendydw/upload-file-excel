187c01a3e37d933169234777f42d55e0ba2567f5
Frendy <frendydw@gmail.com>
2021-11-10 15:33:39 +0700

    upload excel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'upload-file-excel';
+  filename: string;
+  data: any;
+
+  onFileChange(event: any) {
+    const start = performance.now();
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(event.target);
+    this.filename = event.target.files[0].name;
+    if (target.files.length !== 1) {
+      throw new Error('Cannot use multiple files');
+    }
+    const reader: FileReader = new FileReader();
+    reader.readAsBinaryString(target.files[0]);
+    reader.onload = (e: any) => {
+      /* create workbook */
+      const binarystr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(binarystr, { type: 'binary' });
+
+      /* selected the first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      this.data = XLSX.utils.sheet_to_json(ws); // to get 2d array pass 2nd parameter as object {header: 1}
+      console.log(this.data); // Data will be logged in array format containing objects
+      const end = performance.now();
+      console.log(end - start);
+    };
+  }
+
+  getHeaders() {
+    let headers: string[] = [];
+    if(this.data) {
+      this.data.forEach((value) => {
+        Object.keys(value).forEach((key) => {
+          if(!headers.find((header) => header == key)){
+            headers.push(key)
+          }
+        })
+      })
+    }
+    return headers;
+  }
 }
